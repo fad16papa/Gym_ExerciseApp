d87a8978f36e308498560cd7e39717ca87b7bb52
fad16papa <fad16papa@gmail.com>
2022-06-16 18:13:02 +0800

    Commit changes

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
   return (
@@ -12,6 +12,18 @@ const HeroBanner = () => {
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={3}>
+        Checkout the most effective exercises
+      </Typography>
+      <Button variant="contained" color="error">
+        Explore Exercises
+      </Button>
     </Box>
   );
 };
